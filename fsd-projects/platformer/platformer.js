e20f14265f356a44deb6b8e9e1a4c7be39aa0677
Beef500 <oscarr5042053190@gmail.com>
2025-09-25 17:31:51 +0000

    add index.html and css files

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,30 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //  toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
+    createPlatform(400, 600, 200, 20, "blue");
+    createPlatform(500, 200, 250, 60, "yellow");
+    createPlatform(800, 700, 350, 80, "pink");
+    createPlatform(800, 500, 200, 50, "purple");
+    createPlatform(1100, 400, 140, 30, "black");
+    createPlatform(800, 300, 50, 60, "green");
+    createPlatform(900, 350, 20, 40, "orange");
 
 
     // TODO 3 - Create Collectables
-
-
-
+    createCollectable("max", 420, 60, 1.7, 0.7);
+    createCollectable("kennedi", 800, 70, 2.0, 1.0);
+    createCollectable("grace", 1100, 40, 3.0, 1.0);
+    createCollectable("steve", 350, 50);
+    createCollectable("diamond", 1160, 700);
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("top", 300, 450);
+    createCannon("left", 310, 1050);
+    createCannon("right", 100, 500);
     
     
     //////////////////////////////////
